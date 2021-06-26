bf06eea6953c73e8fbd7dbb7749a07b87ea8541b
Kiran Rana <kiranrana8973@gmail.com>
2021-06-26 18:00:14 +0545

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/model/Employee.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerview.model
+
+data class Employee(
+    val name: String? = null,
+    val address: String? = null,
+    val salary: Float? = null,
+    val imageUrl: String? = null
+)
